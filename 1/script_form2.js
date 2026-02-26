dac12b742d6e6256aa882c63bb2c988d4be5a8d5
Yaroslav <yartes83@gmail.com>
2026-02-26 14:49:20 +0200

    26.02.2026

---
+++ 1/script_form2.js
@@ -0,0 +1,6 @@
+function calculateSum() {
+    let num1 = +document.getElementById("num1").value;
+    let num2 = +document.getElementById("num2").value;
+    let sum = num1 + num2;
+    document.getElementById("result").value = sum;
+}
